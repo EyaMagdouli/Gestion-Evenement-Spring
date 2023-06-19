36ecbd7e42dda08a8568ddfb551a1e4c59f52bc7
emagdouli <emagdouli@vermeg.com>
2023-06-19 10:27:03 +0100

    Evenement api

--- src/main/java/com/example/spring/service/interfaces/IEvenementService.java
+++ src/main/java/com/example/spring/service/interfaces/IEvenementService.java
@@ -1,4 +1,18 @@
 package com.example.spring.service.interfaces;
 
+import com.example.spring.persistance.entities.Evenement;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface IEvenementService {
+
+    List<Evenement> getAllEvents();
+
+    Evenement getEventById(Long id);
+
+    Evenement saveEvenement(Evenement evenement);
+
+    void deleteEventById(Long id);
 }
